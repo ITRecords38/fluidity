11350f03acf32663a0ef751c458df9d41adaaba9
ITRecords <fabrice@itrecords.fr>
2025-05-22 22:16:51 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { checker } from "vite-plugin-checker"
 
 export default defineConfig(() => {
   return {
-    base: "./",
+    base: "/fluidity/",
     build: {
       outDir: "build",
     },
